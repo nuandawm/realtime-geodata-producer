588d893f2651a5b5cf03ed1e4d11d039cee01aba
Giuliano De Rossi <derossi.giuliano@gmail.com>
2022-08-14 21:31:52 +0000

    Configure example consumer using sasl plain auth

--- src/app.py
+++ src/app.py
@@ -1,6 +1,29 @@
 import numpy as np
 import pandas as pd
+from kafka import KafkaProducer
+from jproperties import Properties
+from json import dumps
+
+CONSUMER_CONFIG_KEYS = [
+    'bootstrap_servers', 'security_protocol',
+    'sasl_mechanism', 'sasl_plain_username', 'sasl_plain_password'
+]
+
+value_serializer_callable = lambda x: dumps(x).encode('utf-8')
 
 if __name__ == "__main__":
-    series = pd.Series([1, 3, 5, np.nan, 6, 8])
-    print(series)
+    configs = Properties()
+    with open('config.properties', 'rb') as config_file:
+        configs.load(config_file)
+
+    config_dict = {k: v.data for k, v in configs.items() if k in CONSUMER_CONFIG_KEYS}
+
+    producer = KafkaProducer(value_serializer=value_serializer_callable, **config_dict)
+
+    data = {'test': 'puppaaaaa'}
+
+    producer.send('puppa-topic', value=data)
+
+    producer.close()
+    producer.flush()
+
